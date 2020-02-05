9fdd51154b8efe2331f21b65c54e32f513276ca0
destyarufinaa <pramuditarufinad7@gmail.com>
2020-02-05 14:51:07 +0700

    Add: README.md

---
+++ minggu-01/coba.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+ <title>Form Sederhana Suckittrees.com</title>
+</head>
+<body>
+<h2>Form sederhana Suckittrees.com</h2>
+ <form method = 'post' action= "aksiForm.php">
+  <table>
+   <tr>
+    <td>Nama</td>
+    <td>:</td>
+    <td><input type='text' name='nama'></td>
+   </tr>
+   <tr>
+    <td>Umur</td>
+    <td>:</td>
+    <td><input type='text' name='umur'></td>
+   </tr>
+   <tr>
+    <td>Alamat</td>
+    <td>:</td>
+    <td><input type='text' name='alamat'></td>
+   </tr>
+   <tr>
+    <td></td>
+    <td></td>
+    <td><input type='submit' value='KIRIM'></td>
+   </tr>
+  </table>  
+ </form>
+</body>
+</html>
